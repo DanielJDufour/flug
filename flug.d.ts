f970c617059f19286b32e9f016124351f0bc9245
DanielJDufour <daniel.j.dufour@gmail.com>
2022-05-27 21:09:07 -0400

    fixed type

--- flug.d.ts
+++ flug.d.ts
@@ -1,4 +1,4 @@
 export default function test(
   name: string,
-  callback: ({ eq }: { eq?: (actual: any, expected: any) => boolean }) => void
+  callback: ({ eq }: { eq: (actual: any, expected: any) => boolean }) => void
 ): void;
